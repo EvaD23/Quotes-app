1844bf3b31c143574a7469ecbcd8eaee40d36924
Евгения Лазарева <evgenialazareva@MacBook-Air-Evgenia.local>
2022-06-25 15:23:27 +0300

    add gradient

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function changeColor() {
-    document.body.style = `background-color: rgb(${generateNumber()}, ${generateNumber()}, ${generateNumber()})`;
+    document.body.style = `background: linear-gradient(${generateRGB()}, ${generateRGB()});`;
+
 }
 
 function generateNumber() {
     return Math.round(Math.random() * 255); // сгенерить число от 0 до 255.  
+}
+
+function generateRGB() {
+    return `rgb(${generateNumber()}, ${generateNumber()}, ${generateNumber()})`;
 }
